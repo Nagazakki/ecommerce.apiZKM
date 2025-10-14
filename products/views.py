63337f8dab9930bf495e077164b3d49e6acaa7ed
Nagazakki <106966729+Nagazakki@users.noreply.github.com>
2025-10-14 16:51:11 +0000

    Filtering, Searching and Pagination to make it useful for real queries

--- products/views.py
+++ products/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, filters
 from .models import Product
 from .serializers import ProductSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all().order_by('-created_at')
     serializer_class = ProductSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['category']
+    search_fields = ['name', 'category']
